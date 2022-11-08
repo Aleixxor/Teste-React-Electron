3b6ca2be7ff1096b268fcb020b86d57f42d17d40
Aleixxor <alexlouzada2009@gmail.com>
2022-11-08 10:12:41 -0300

    Commitão do faustão

---
+++ index.js
@@ -0,0 +1,8 @@
+const { BrowserWindow, app } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({ width: 800, height: 600 });
+  win.loadURL("http://localhost:3000");
+};
+
+app.on("ready", createWindow);
